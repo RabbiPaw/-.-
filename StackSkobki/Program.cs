64f6d29ea57b7a2eb3626a95fe6916bfde8119bd
Святослав Салабун <124811370+RabbiPaw@users.noreply.github.com>
2023-05-15 22:39:24 +0600

    Add files via upload

---
+++ StackSkobki/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackSkobki
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var Skobki = new Stack<char>();
+            string b = Console.ReadLine();
+            foreach (char s in b){Skobki.Push(s);}
+            List<char> s1 = new List<char>();
+            List<char> s2 = new List<char>();
+            List<char> s3 = new List<char>();
+            foreach (char s in Skobki)
+            {
+                if (s =='(' || s == ')')
+                {
+                    s1.Add(s);
+                }
+                if (s =='{' || s == '}')
+                {
+                    s2.Add(s);
+                }
+                if (s =='[' || s == ']')
+                {
+                    s3.Add(s);
+                }
+            }
+            if (((s1.FindAll(t=>t=='(')).Count() <= (s1.FindAll(t=>t==')')).Count()) && ((s2.FindAll(t => t == '{')).Count() <= (s2.FindAll(t => t == '}')).Count()) && ((s3.FindAll(t => t == '[')).Count() <= (s3.FindAll(t => t == ']')).Count()))
+            {
+                Console.WriteLine("Все скобки закрыты");
+            }
+            else { Console.WriteLine("Не все скобки закрыты"); }
+        }
+    }
+}
