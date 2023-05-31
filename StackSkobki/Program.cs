2549c17cceee73fe68ade20b1ffd3f19d3b1e7eb
Святослав Салабун <124811370+RabbiPaw@users.noreply.github.com>
2023-05-31 23:33:43 +0600

    Add files via upload

---
+++ StackSkobki/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackSkobki
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var Skobki = new Stack<char>();
+            string b = Console.ReadLine();
+            foreach (char s in b)
+            {
+                if (s == '(' || s == '{' || s == '[')
+                {
+                    Skobki.Push(s);
+                }
+                else if (Skobki.Peek() == '(' && s == ')')
+                {
+                    Skobki.Pop();
+                }
+                else if (Skobki.Peek() == '{' && s == '}')
+                {
+                    Skobki.Pop();
+                }
+                else if (Skobki.Peek() == '[' && s == ']')
+                {
+                    Skobki.Pop();
+                }
+                else
+                {
+                    Console.WriteLine("Не все скобки закрыты");
+                    Environment.Exit(0);
+                }
+            }
+
+            if (Skobki.Count() == 0)
+            {
+                Console.WriteLine("Все скобки закрыты");
+            }
+            else
+            {
+                Console.WriteLine("Не все скобки закрыты");
+            }
+
+            } 
+        }
+    }
